dd4c47ae7d9806e69c5a57411530538f4dcf29c6
Max Konkin <mirk.mad@gmail.com>
2024-03-25 01:57:27 +0400

    fix: getById method added

--- src/main/java/space/maxkonkin/nasapicbot/service/UserService.java
+++ src/main/java/space/maxkonkin/nasapicbot/service/UserService.java
@@ -4,16 +4,20 @@ import space.maxkonkin.nasapicbot.model.User;
 import space.maxkonkin.nasapicbot.repository.UserDao;
 
 import java.util.List;
+import java.util.Optional;
 
 public class UserService {
     private final UserDao repository;
-
     public UserService(UserDao repository) {
         this.repository = repository;
     }
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public Optional<User> getById(long id) {
+        return repository.getById(id);
     }
 
     public void save(User user) {
