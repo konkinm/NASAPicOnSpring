bb5c38d9630740f69e28d2eb96d8775b3365810f
Max Konkin <mirk.mad@gmail.com>
2024-03-25 01:52:37 +0400

    chore: cleanup

--- src/test/java/space/maxkonkin/nasapicbot/HandlerTest.java
+++ src/test/java/space/maxkonkin/nasapicbot/HandlerTest.java
@@ -2,9 +2,11 @@ package space.maxkonkin.nasapicbot;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import space.maxkonkin.nasapicbot.model.QueueMessage;
 import space.maxkonkin.nasapicbot.web.Handler;
 
+@Slf4j
 public class HandlerTest {
     public static void main(String[] args) throws JsonProcessingException {
         final String MESSAGE = """
@@ -39,41 +41,10 @@ public class HandlerTest {
                   ]
                 }
                 """;
-        final String MESSAGE_2 =
-                """
-                        {
-                            "messages": [
-                                {
-                                    "event_metadata": {
-                                        "event_id": "cce76685-5828-4304-a83d-95643c0507a0",
-                                        "event_type": "yandex.cloud.events.messagequeue.QueueMessage",
-                                        "created_at": "2019-09-24T00:54:28.980441Z"
-                                    },
-                                    "details": {
-                                        "queue_id": "yrn:yc:ymq:ru-central1:21i6v06sqmsaoeon7nus:event-queue",
-                                        "message": {
-                                            "message_id": "cce76685-5828-4304-a83d-95643c0507a0",
-                                            "md5_of_body": "d29343907090dff4cec4a9a0efb80d20",
-                                            "body": "{\\"update_id\\":10281888,\\"message\\":{\\"message_id\\":252,\\"from\\":{\\"id\\":229590625,\\"is_bot\\":false,\\"first_name\\":\\"Max\\",\\"username\\":\\"Mfx_m\\",\\"language_code\\":\\"en\\"},\\"chat\\":{\\"id\\":229590625,\\"first_name\\":\\"Max\\",\\"username\\":\\"Mfx_m\\",\\"type\\":\\"private\\"},\\"date\\":1695296606,\\"text\\":\\"/start\\",\\"entities\\":[{\\"offset\\":0,\\"length\\":6,\\"type\\":\\"bot_command\\"}]}}",
-                                            "attributes": {
-                                                "SentTimestamp": "1569285804456"
-                                            },
-                                            "message_attributes": {
-                                                "messageAttributeKey": {
-                                                    "dataType": "StringValue",
-                                                    "stringValue": "value"
-                                                }
-                                            },
-                                            "md5_of_message_attributes": "83eb2d0afefb150c1ffe69f66c3de068"
-                                        }
-                                    }
-                                }
-                            ]
-                        }
-                """;
+
         ObjectMapper mapper = new ObjectMapper();
         Handler handler = new Handler();
-        String message = handler.apply(mapper.readValue(MESSAGE_2, QueueMessage.class));
-        System.out.println("Message: " + mapper.writeValueAsString(message));
+        String status = handler.apply(mapper.readValue(MESSAGE, QueueMessage.class));
+        log.info("Status: " + mapper.writeValueAsString(status));
     }
 }
