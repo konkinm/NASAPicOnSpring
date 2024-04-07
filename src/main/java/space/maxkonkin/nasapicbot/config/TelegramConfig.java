eeb2f7c6833c138138316cfcaa11891a8382d99a
Max Konkin <mirk.mad@gmail.com>
2024-04-07 18:15:59 +0400

    chore: imports optimized

--- src/main/java/space/maxkonkin/nasapicbot/config/TelegramConfig.java
+++ src/main/java/space/maxkonkin/nasapicbot/config/TelegramConfig.java
@@ -4,15 +4,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.PropertiesPropertySource;
-import org.springframework.core.io.support.EncodedResource;
-import org.springframework.core.io.support.PropertySourceFactory;
-
-import java.util.Objects;
-import java.util.Properties;
 
 @Configuration
 @PropertySource(value = "classpath:application-${SPRING_PROFILE}.yaml", factory = YamlPropertySourceFactory.class)
