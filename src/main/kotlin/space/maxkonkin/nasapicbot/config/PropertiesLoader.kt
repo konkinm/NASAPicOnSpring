00a266e9b337d9a9a707defce558793b26da396e
MaxKonkin <knkn.max@yandex.ru>
2025-09-03 00:54:40 +0300

    refactor: PropertiesLoader refactor

--- src/main/kotlin/space/maxkonkin/nasapicbot/config/PropertiesLoader.kt
+++ src/main/kotlin/space/maxkonkin/nasapicbot/config/PropertiesLoader.kt
@@ -2,13 +2,17 @@ package space.maxkonkin.nasapicbot.config
 
 import org.yaml.snakeyaml.Yaml
 import java.io.FileNotFoundException
+import java.io.InputStream
 
 object PropertiesLoader {
     fun loadProperties(): Map<String, Any> {
         val resourcePath = "/application-${System.getenv("SPRING_PROFILE")}.yaml"
-        this::class.java.getResourceAsStream(resourcePath)?.use { inputStream ->
+        val inputStream: InputStream = this::class.java.getResourceAsStream(resourcePath)
+            ?: throw FileNotFoundException("Resource not found: $resourcePath")
+        
+        return inputStream.use { stream ->
             val yaml = Yaml()
-            return yaml.load(inputStream) ?: emptyMap()
-        } ?: throw FileNotFoundException("Resource not found: $resourcePath")
+            yaml.load(stream) ?: emptyMap()
+        }
     }
 }
